106a6752ad45861e725954146eef6d2218f7512c
saeed-khalafinejad <saeedkh0@yahoo.com>
2020-01-07 16:08:44 +0330

    Shared and open source license added.

--- app/src/main/java/org/kasabeh/androidprint/sampleprint/MainActivity.java
+++ app/src/main/java/org/kasabeh/androidprint/sampleprint/MainActivity.java
@@ -1,19 +1,7 @@
 package org.kasabeh.androidprint.sampleprint;
 /*
+MIT License
 Copyright (C) 2015/1393  <Saeed Khalafinejad, Kasabeh group>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 You may contact-us via info@kasabeh.org.
 */
